21857daea9d9dd37baf13f821c26927335029dbf
wwwangxc <w.xingchen@outlook.com>
2022-06-14 14:15:14 +0800

    update fmt package

--- lua/customer/lsp/null-ls.lua
+++ lua/customer/lsp/null-ls.lua
@@ -11,10 +11,10 @@ local diagnostics = null_ls.builtins.diagnostics
 null_ls.setup({
 	debug = false,
 	sources = {
-		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-		formatting.black.with({ extra_args = { "--fast" } }),
-		formatting.stylua,
-    -- diagnostics.flake8
+		--formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+		--formatting.black.with({ extra_args = { "--fast" } }),
+		--formatting.stylua,
+    --diagnostics.flake8
 
     diagnostics.golangci_lint,
 	},
