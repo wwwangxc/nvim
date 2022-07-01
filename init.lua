3130d2b2a3b0883726480a9edbbfe7a931c4c87e
wwwangxc <w.xingchen@outlook.com>
2022-07-01 11:09:27 +0800

    增加备注

--- init.lua
+++ init.lua
@@ -1,25 +1,23 @@
 local modules = {
   -- core
-  "core.options",
-  "core.autocmds",
-  "core.mappings",
+  "core.options",               -- Neovim 配置
+  "core.autocmds",              -- 自动执行的命令
+  "core.mappings",              -- 快捷键
 
-  -- plugins
-  "customer.plugins",
-
-  "customer.cmp",
-  "customer.lsp",
-  "customer.vim-go",
-
-  "customer.colorscheme",
-  "customer.treesitter",
-  "customer.telescope",
-  "customer.gitsigns",
-  "customer.nerdtree",
-  "customer.whichkey",
-  "customer.lualine",
-  --"customer.autopairs",
-  "customer.indent-blankline",
+  -- customer
+  "customer.plugins",           -- 插件管理
+  "customer.cmp",               -- 自动完成
+  "customer.lsp",               -- Languase Server Protocol
+  "customer.vim-go",            -- Golang 语言支持
+  "customer.colorscheme",       -- 主题
+  "customer.treesitter",        -- 代码高亮
+  "customer.telescope",         -- 搜索
+  "customer.gitsigns",          -- Git
+  "customer.nerdtree",          -- 目录树
+  "customer.whichkey",          -- 快捷键指令辅助
+  "customer.lualine",           -- 状态栏
+  "customer.autopairs",         -- 自动完成括号
+  "customer.indent-blankline",  -- 代码辅助线
 }
 
 for _, module in ipairs(modules) do
